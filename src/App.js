16d0c9adb6ae882680262660dfc607521534095f
Biswajit Paul <biswajitkmrpl46@gmail.com>
2021-05-01 14:50:18 +0530

    Custom Component Dynamic Values added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const ExpenseItems = [
+    {
+      title: "MacBook Air M1",
+      date: new Date(2021, 3, 25),
+      amount: 1600,
+    },
+    {
+      title: "Iphone 12 mini",
+      amount: 600,
+      date: new Date(2021, 3, 26),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={ExpenseItems[0].title}
+        amount={ExpenseItems[0].amount}
+        date={ExpenseItems[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={ExpenseItems[1].title}
+        amount={ExpenseItems[1].amount}
+        date={ExpenseItems[1].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
+function ExpenseItem(data){
+
+    const vYear=data.date.toLocaleString('default',{year:'numeric'});
+    const vMonth=data.date.toLocaleString('default',{month:'long'});
+    const vDay=data.date.toLocaleString('default',{day:'2-digit'});
+
     return(
         <div className="expense-item">
-            <div>01 May 2021</div>
-            <div className="expense-item__description">
-                <h2>MacBook Air M1</h2>
-                <div className="expense-item__price">$1600</div>
+            <div>
+                <div>{vYear}</div>
+                <div>{vMonth}</div>
+                <div>{vDay}</div>
             </div>
-
+            <div className="expense-item__description">
+                <h2>{data.title}</h2>
+                <div className="expense-item__price">${data.amount}</div>
+            </div>
         </div>
     );
 }
